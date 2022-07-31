7526430b22a728be22dd9451f259713be80f2f36
Ootopi <94833201+Ootopi@users.noreply.github.com>
2022-07-31 14:39:15 +0800

    Update cycleforlife-sg.js

--- api/cycleforlife-sg.js
+++ api/cycleforlife-sg.js
@@ -1,7 +1,8 @@
 export default async function handler(request, response) {
   const fetch = require('node-fetch')
   const parse = require('node-html-parser').parse
-  fetch('https://serverless-cors.vercel.app/api/cors?url=https://www.cycleforhope.sg/user/188908')
+  const id = request.query.id
+  fetch(`https://serverless-cors.vercel.app/api/cors?url=https://www.cycleforhope.sg/user/${id}`)
       .then(r => r.text())
       .then(raw => parse(raw))
       .then(root => root.querySelectorAll('div.w-full.rounded-md.border.py-2.border-gray-400'))
@@ -10,5 +11,5 @@ export default async function handler(request, response) {
           amount: card.querySelector('p:nth-of-type(2) span').textContent,
           message: card.querySelector('p:nth-of-type(3)').textContent,
       })))
-      .then(json => response.send(json))
+      .then(json => response.json(json))
 }
