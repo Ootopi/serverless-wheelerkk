be472003b5e329a8055596d89e7aded04c85f6ca
Ootopi <94833201+Ootopi@users.noreply.github.com>
2022-07-31 13:26:18 +0800

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,13 @@
+export default async function handler(request, response) {
+
+  fetch('https://serverless-cors.vercel.app/api/cors?url=https://www.cycleforhope.sg/user/188908')
+      .then(r => r.text())
+      .then(raw => parse(raw))
+      .then(root => root.querySelectorAll('div.w-full.rounded-md.border.py-2.border-gray-400'))
+      .then(cards => cards.map(card => ({
+          name: card.querySelector('p:nth-of-type(1)').textContent,
+          amount: card.querySelector('p:nth-of-type(2) span').textContent,
+          message: card.querySelector('p:nth-of-type(3)').textContent,
+      })))
+      .then(json => response.send(json))
+}
